0235c6f18ad920a1cfbb6442836e24117fbff777
Jonathan M. Wilbur <jonathan@wilbur.space>
2018-12-22 22:55:14 -0500

    Added UnboundedDirectoryString to exports

--- dist/types/SelectedAttributeTypes/Version8/index.d.ts
+++ dist/types/SelectedAttributeTypes/Version8/index.d.ts
@@ -1,3 +1,4 @@
 import { ObjectIdentifier } from "asn1-ts";
 export declare const selectedAttributeTypesVersion8OID: ObjectIdentifier;
+export { default as UnboundedDirectoryString } from "./UnboundedDirectoryString";
 export { default as UniqueIdentifier } from "./UniqueIdentifier";
